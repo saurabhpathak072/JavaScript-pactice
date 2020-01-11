292b98ceb46c0f51ee1a561c578974a86b29f8eb
saurabhpathak072 <saurabhpathak072@gmail.com>
2020-01-11 22:04:46 +0530

    Logical and comparitive operator

---
+++ six.js
@@ -0,0 +1,39 @@
+var a=11
+undefined
+a<11
+false
+a>11
+false
+a>10
+true
+a<10
+false
+a<=12
+true
+a>=19
+false
+
+var a = 10
+undefined
+a!==10
+false
+a!==11
+true
+a!=11
+true
+
+var a = true
+undefined
+!a
+false
+var b =!a
+undefined
+b
+false
+!b
+true
+
+
+true && true => true
+true && false => false
+true || false=> true
