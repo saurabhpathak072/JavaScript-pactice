2eab9e83a19ff15d93e6003bc2dac39409ca0671
saurabhpathak072 <saurabhpathak072@gmail.com>
2020-01-11 17:55:17 +0530

    Assign operator and user input

---
+++ Second.js
@@ -0,0 +1,61 @@
+=       ->  Assignment
+==      ->  Comparing just value
+===     ->  comparing data and data type
+
+var a = 10
+var b == 10
+var c === "10"
+
+
+a==220
+false
+a == "10"
+true
+a==="10"
+false
+
+
+
+var : value override everytime don't keep static value 
+
+in ES5 we have only one way to declare variable if.e. var 
+
+in ES6 we have 2 way to declare variable 
+
+let and const 
+
+var a= 20  two line code for java Script 
+var a declare
+a=20 Assignment
+
+
+a
+20
+a=30
+30
+
+let c=70
+undefined
+c
+70
+let c=90
+SyntaxError: redeclaration of let c
+debugger eval code:1:1
+c=90
+90
+
+const a = 30
+undefined
+a
+30
+const a = 20
+SyntaxError: redeclaration of const a
+debugger eval code:1:1
+a=20
+TypeError: invalid assignment to const `a';
+
+
+
+var = redeclare and reassign
+let = cannot redeclare but can reaasign
+const = cannot reassign and cannot redeclare
---
+++ third.js
@@ -0,0 +1,50 @@
+console.log()  -> used to debug the project 
+shows message in the console
+
+Alert -> is like a prompt , 
+shoows just message in pop up prompt
+
+confirm -> return Boolean value on the basis of user input
+ok-> true
+cancel -> false
+used to condition
+
+prompt -> its used to get data from user it string FormData
+
+
+console.log("hii")
+VM186:1 hii
+undefined
+alert("hii")
+undefined
+confirm("Do you want to leave the button")
+true
+confirm("Do you want to leave the button")
+false
+var a = confirm("Do you want to leave the button")
+undefined
+a
+true
+prompt("please tell me your name")
+""
+prompt("please tell me your name")
+"sauabh"
+let name=prompt("please tell me your name")
+undefined
+name
+"Amit"
+
+Q. 
+
+var a = prompt("Please tell me One Number")
+var b = prompt("Please tell me sec Number")
+
+undefined
+console.log(a+b)
+ 1245
+ *takes input in string FormData
+
+
+console.log(parseInt(a)+parseInt(b))
+ 57
+
