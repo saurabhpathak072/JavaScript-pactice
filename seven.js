e50815321ec4718bbbf9f997de5fdc4d6b15a0a6
saurabhpathak072 <saurabhpathak072@gmail.com>
2020-01-11 22:17:47 +0530

    Conditional

---
+++ seven.js
@@ -0,0 +1,25 @@
+var a = 10
+
+if (a%2==0)
+{
+    console.log("Number is Even")
+}
+else if(a%3==0)
+{
+    console.log("Number is divisible by 3")
+}
+else
+{
+    console.log("Number is odd")
+}
+
+///////////ternary operater//////////
+
+var a=9
+undefined
+var b = a<10?"less than 10":"greater than 10"
+undefined
+b
+"less than 10"
+
+if condition before question mark is true then first part will execute else second part will execute 
